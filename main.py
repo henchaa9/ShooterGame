cfe7806ad21e4def62dc682eb0d3c3b2a0b7f987
henchaa9 <bonis.obo@gmail.com>
2021-01-20 17:21:50 +0200

    added spawning enemies and proper hit detection

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import pygame, sys
+import pygame, sys, random
 
+pygame.init()
 
 width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 
-#COLORS
+# COLORS
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 yellow = (255, 255, 0)
 
-#MOVEMENT VALUES
+# MOVEMENT VALUES
 player_speed = 3
 enemy_speed = 2
-bullet_speed = 5
+bullet_speed = 4
+
+# USER EVENTS
+REMOVEBULLET = pygame.USEREVENT + 0
+SPAWNENEMY = pygame.USEREVENT + 1
+
+pygame.time.set_timer(SPAWNENEMY, 2000)
 
 
 def draw(*args):
     win.fill(white)
 
     pygame.draw.rect(win, green, player)
-    pygame.draw.rect(win, red, enemy)
+    
+    for enemy in enemies:
+        pygame.draw.rect(win, red, enemy)
 
     for bullet in bullets:
         pygame.draw.rect(win, yellow, bullet)
@@ -31,11 +40,11 @@ def draw(*args):
 def move_player(keys_pressed, player):
 
     if keys_pressed[pygame.K_w]:
-        if player.y > 1:
+        if player.y > 4:
             player.y -= player_speed
 
     if keys_pressed[pygame.K_s]:
-        if player.y < height - 11:
+        if player.y < height - 24:
             player.y += player_speed
 
     if keys_pressed[pygame.K_a]:
@@ -43,25 +52,27 @@ def move_player(keys_pressed, player):
             player.x -= player_speed
 
     if keys_pressed[pygame.K_d]:
-        if player.x < width - 11:
+        if player.x < width - 24:
             player.x += player_speed
     
 
-def enemy_attack(player, enemy):
+def enemy_attack(player):
 
-    if enemy.x < player.x:
-        enemy.x += enemy_speed
-    else:
-        enemy.x -= enemy_speed
+    for enemy in enemies:
 
-    if enemy.y < player.y:
-        enemy.y += enemy_speed
-    else:
-        enemy.y -= enemy_speed
+        if enemy.x < player.x:
+            enemy.x += enemy_speed
+        else:
+            enemy.x -= enemy_speed
 
-    if player.colliderect(enemy):
-        print("You lost")
-        sys.exit()
+        if enemy.y < player.y:
+            enemy.y += enemy_speed
+        else:
+            enemy.y -= enemy_speed
+
+        if player.colliderect(enemy):
+            print("You lost")
+            sys.exit()
 
 
 class Bullet(pygame.Rect):
@@ -71,6 +82,7 @@ class Bullet(pygame.Rect):
         self.vel = bullet_speed
         self.dir = pygame.math.Vector2(targetdir) - self.pos
         self.dir = self.dir.normalize()
+        self.collisions = 0
 
 
 def shoot(bullets):
@@ -81,21 +93,27 @@ def shoot(bullets):
             bullet.pos += bullet.dir * bullet.vel
             bullet.x, bullet.y = (round(bullet.pos.x), round(bullet.pos.y))
         else:
-            bullets.remove(bullet)
+            pygame.event.post(pygame.event.Event(REMOVEBULLET))
+            
+        for enemy in enemies:
 
-        if bullet.colliderect(enemy):
-            bullets.remove(bullet)
-            print('hit')
+            if enemy.colliderect(bullet):
+                pygame.event.post(pygame.event.Event(REMOVEBULLET))
+                enemies.remove(enemy)
+
+                
+def spawn_enemy():
+    enemy = pygame.Rect(random.randint(1, 800), random.randint(1, 600), 20, 20)
+    enemies.append(enemy)
 
 
 fps = 60
 clock = pygame.time.Clock()
 
 player = pygame.Rect(30, 40, 20, 20)
-enemy = pygame.Rect(300, 300, 20, 20)
 
 bullets = []
-bullet_pos = []
+enemies = []
 
 
 while True:
@@ -110,9 +128,15 @@ while True:
             mx, my = pygame.mouse.get_pos()
             bullets.append(Bullet(player.x, player.y, 10, 10, (mx, my)))
             
+        if event.type == SPAWNENEMY:
+            spawn_enemy()
+        
+        if event.type == REMOVEBULLET:
+            bullets.pop(-1)
+
     keys_pressed = pygame.key.get_pressed() 
 
-    draw(player, enemy)
+    draw(player)
     move_player(keys_pressed, player)
-    enemy_attack(player, enemy)
+    enemy_attack(player)
     shoot(bullets)
