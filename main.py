4aef04aceaa889fa30102d50039ac943ee924421
henchaa9 <bonis.obo@gmail.com>
2021-01-23 19:46:54 +0200

    Refactored code for scenes

--- main.py
+++ main.py
@@ -2,140 +2,180 @@ import pygame, sys, random
 
 pygame.init()
 
-width, height = 800, 600
-win = pygame.display.set_mode((width, height))
+# MAIN WINDOW
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # COLORS
-white = (255, 255, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-yellow = (255, 255, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 # MOVEMENT VALUES
-player_speed = 3
-enemy_speed = 2
-bullet_speed = 4
+PLAYER_SPEED = 3
+ENEMY_SPEED = 2
+BULLET_SPEED = 4
 
 # USER EVENTS
 REMOVEBULLET = pygame.USEREVENT + 0
 SPAWNENEMY = pygame.USEREVENT + 1
 
-pygame.time.set_timer(SPAWNENEMY, 2000)
-
-
-def draw(*args):
-    win.fill(white)
-
-    pygame.draw.rect(win, green, player)
-    
-    for enemy in enemies:
-        pygame.draw.rect(win, red, enemy)
-
-    for bullet in bullets:
-        pygame.draw.rect(win, yellow, bullet)
-
-    pygame.display.update()
-
-
-def move_player(keys_pressed, player):
-
+# PLAYER MOVEMENT
+def player_movement():
     if keys_pressed[pygame.K_w]:
         if player.y > 4:
-            player.y -= player_speed
+            player.y -= PLAYER_SPEED
 
     if keys_pressed[pygame.K_s]:
-        if player.y < height - 24:
-            player.y += player_speed
+        if player.y < HEIGHT - 24:
+            player.y += PLAYER_SPEED
 
     if keys_pressed[pygame.K_a]:
         if player.x > 3:
-            player.x -= player_speed
+            player.x -= PLAYER_SPEED
 
     if keys_pressed[pygame.K_d]:
-        if player.x < width - 24:
-            player.x += player_speed
-    
-
-def enemy_attack(player):
-
-    for enemy in enemies:
-
-        if enemy.x < player.x:
-            enemy.x += enemy_speed
-        else:
-            enemy.x -= enemy_speed
-
-        if enemy.y < player.y:
-            enemy.y += enemy_speed
-        else:
-            enemy.y -= enemy_speed
-
-        if player.colliderect(enemy):
-            print("You lost")
-            sys.exit()
+        if player.x < WIDTH - 24:
+            player.x += PLAYER_SPEED
 
 
+# BULLET
 class Bullet(pygame.Rect):
     def __init__(self, left, top, width, heigth, targetdir):
         super().__init__(left, top, width, heigth)
         self.pos = pygame.math.Vector2((left, top))
-        self.vel = bullet_speed
+        self.vel = BULLET_SPEED
         self.dir = pygame.math.Vector2(targetdir) - self.pos
         self.dir = self.dir.normalize()
-        self.collisions = 0
 
 
-def shoot(bullets):
+# GAME SCENES
+class Scenes():
+    pygame.time.set_timer(SPAWNENEMY, 2000)
 
-    for bullet in bullets:
+    def __init__(self):
+        self.scene = 'scene_1'
+        self.bullets = []
+        self.enemies = []
 
-        if bullet.x < width and bullet.x > 0 and bullet.y < height and bullet.y > 0:
-            bullet.pos += bullet.dir * bullet.vel
-            bullet.x, bullet.y = (round(bullet.pos.x), round(bullet.pos.y))
-        else:
-            pygame.event.post(pygame.event.Event(REMOVEBULLET))
-            bullets.remove(bullet)
-            
-        for enemy in enemies:
 
-            if enemy.colliderect(bullet):
-                pygame.event.post(pygame.event.Event(REMOVEBULLET))
-                bullets.remove(bullet)
-                enemies.remove(enemy)
+    def scene_manager(self):
+        if self.scene == 'scene_1':
+            self.scene_1()
 
+        elif self.scene == 'scene_2':
+            self.scene_2()
+
+
+    def scene_1(self):      
+        # EVENTS
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                self.bullets.append(Bullet(player.x, player.y, 10, 10, (mx, my)))
                 
-def spawn_enemy():
-    enemy = pygame.Rect(random.randint(1, 800), random.randint(1, 600), 20, 20)
-    enemies.append(enemy)
+            if event.type == SPAWNENEMY:
+                enemy = pygame.Rect(random.randint(1, 800), random.randint(1, 600), 20, 20)
+                self.enemies.append(enemy)
+
+        # PLAYER MOVEMENT
+        player_movement()
+
+        # CHECKS FOR SCENE SWITCH
+        if keys_pressed[pygame.K_d] and player.x == 777:
+            player.x = 6
+            self.scene = 'scene_2'
+
+        # DRAWING TO SCREEN
+        win.fill(WHITE)
+
+        pygame.draw.rect(win, GREEN, player)
+        
+        for enemy in self.enemies:
+            pygame.draw.rect(win, RED, enemy)
+
+        for bullet in self.bullets:
+            pygame.draw.rect(win, YELLOW, bullet)
+
+        pygame.display.update()
+        
+        # ENEMIES TRACKING PLAYER
+        for enemy in self.enemies:
+
+            if enemy.x < player.x:
+                enemy.x += ENEMY_SPEED
+            else:
+                enemy.x -= ENEMY_SPEED
+
+            if enemy.y < player.y:
+                enemy.y += ENEMY_SPEED
+            else:
+                enemy.y -= ENEMY_SPEED
+
+            if player.colliderect(enemy):
+                print("You lost")
+                sys.exit()
+
+        # BULLET TRACKING
+        for bullet in self.bullets:
+            if bullet.x < WIDTH and bullet.x > 0 and bullet.y < HEIGHT and bullet.y > 0:
+                bullet.pos += bullet.dir * bullet.vel
+                bullet.x, bullet.y = (round(bullet.pos.x), round(bullet.pos.y))
+            else:
+                pygame.event.post(pygame.event.Event(REMOVEBULLET))
+                self.bullets.remove(bullet)
+                
+            for enemy in self.enemies:
+                if enemy.colliderect(bullet):
+                    pygame.event.post(pygame.event.Event(REMOVEBULLET))
+                    self.bullets.remove(bullet)
+                    self.enemies.remove(enemy)
 
 
-fps = 60
-clock = pygame.time.Clock()
+    def scene_2(self):
+        # EVENTS
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
+        # CHECKS FOR SCENE SWITCH
+        if keys_pressed[pygame.K_a] and player.x == 3:
+            player.x = 777
+            self.scene = 'scene_1'
+            self.bullets = []
+            self.enemies = []
+
+        # PLAYER MOVEMENT
+        player_movement()
+
+        # DRAWING TO SCREEN
+        win.fill(WHITE)
+
+        pygame.draw.rect(win, GREEN, player)
+
+        pygame.display.update()
+
+
+# PLAYER
 player = pygame.Rect(30, 40, 20, 20)
 
-bullets = []
-enemies = []
+# SCENE INSTANCE
+current_scene = Scenes()
 
+# MAIN LOOP
+FPS = 60
+clock = pygame.time.Clock()
 
 while True:
-    clock.tick(fps)
-    
-    for event in pygame.event.get():
+    clock.tick(FPS)
 
-        if event.type == pygame.QUIT:
-            sys.exit()
+    keys_pressed = pygame.key.get_pressed()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mx, my = pygame.mouse.get_pos()
-            bullets.append(Bullet(player.x, player.y, 10, 10, (mx, my)))
-            
-        if event.type == SPAWNENEMY:
-            spawn_enemy()
+    # SCENE SWITCHING
+    current_scene.scene_manager()
 
-    keys_pressed = pygame.key.get_pressed() 
 
-    draw(player)
-    move_player(keys_pressed, player)
-    enemy_attack(player)
-    shoot(bullets)
